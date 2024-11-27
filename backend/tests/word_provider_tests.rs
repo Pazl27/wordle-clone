59ca1d343e1f54a2170bf449d4121850c562ce76
paul <jaul.ponas@gmail.com>
2024-11-27 11:25:56 +0100

    tests for backend - integration tests do not work

--- backend/tests/word_provider_tests.rs
+++ backend/tests/word_provider_tests.rs
@@ -1,11 +1,16 @@
-// use backend::word_provider::find_same_letters;
+// use crate::word_provider::find_same_letters;
 
-// #[test]
-// fn test_find_same_letters() {
-//     let word = "hello";
-//     let guess = "world";
-//     let result = find_same_letters(word, guess);
-//     assert_eq!(result.len(), 2);
-//     assert_eq!(result.get(&1), Some(&'o'));
-//     assert_eq!(result.get(&3), Some(&'l'));
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_find_same_letters() {
+//         let word = "hello";
+//         let guess = "world";
+//         let result = find_same_letters(word, guess);
+//         assert_eq!(result.len(), 2);
+//         assert_eq!(result.get(&1), Some(&'o'));
+//         assert_eq!(result.get(&3), Some(&'l'));
+//     }
 // }
